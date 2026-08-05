b41544580b97e7ef801d5ecf587c8e81db7f3594
BlackShima <blackshima02@gmail.com>
2026-08-05 16:42:28 +0700

    lab05-2

--- Pinia-Store/src/App.vue
+++ Pinia-Store/src/App.vue
@@ -8,7 +8,7 @@ const { message } = storeToRefs(store)
 </script>
 
 <template>
-  <div id="layout">
+  <div class="text-center font-sans text-gray-700 antialiased">
     <header>
       <!-- Fixed typo: id="flashMessage" -->
       <div id="flashMessage" v-if="message">
@@ -31,14 +31,6 @@ const { message } = storeToRefs(store)
 </template>
 
 <style>
-#layout {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 nav {
   padding: 30px;
 }
