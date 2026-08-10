c69fec3f2e681ba0cfb8128b8b07b92cf974ec59
BlackShima <blackshima02@gmail.com>
2026-08-10 16:36:03 +0700

    hello

--- Pinia-Store/src/App.vue
+++ Pinia-Store/src/App.vue
@@ -9,7 +9,7 @@ const { message } = storeToRefs(store)
 </script>
 
 <template>
-  <SpeedInsights />
+  <SpeedInsights /> //hello
   <div class="text-center font-sans text-gray-700 antialiased">
     <header>
       <div
